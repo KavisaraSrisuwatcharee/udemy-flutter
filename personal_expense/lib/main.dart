75fb3e6c09098ee189e4aedd48fc489f507444dc
Chingly <kavisara.noya@mail.kmutt.ac.th>
2021-03-03 17:36:31 +0700

    [ADD] meal_app

--- personal_expense/lib/main.dart
+++ personal_expense/lib/main.dart
@@ -118,6 +118,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    print('build() TransactionList');
     final mediaQuery = MediaQuery.of(context);
     final isLandscape = mediaQuery.orientation == Orientation.landscape;
     final PreferredSizeWidget appBar = Platform.isIOS
