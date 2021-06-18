b8efbd9759e3a9870d833e404c72d7606c134334
Chingly <kavisara.noya@mail.kmutt.ac.th>
2021-06-18 23:51:45 +0700

    [ADD] shop app

---
+++ shop_app/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/screens/products_overview_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Myshop',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: ProductsOverviewScreen(),
+    );
+  }
+}
