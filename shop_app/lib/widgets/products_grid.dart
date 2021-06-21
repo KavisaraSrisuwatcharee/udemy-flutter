f3936bc28947e8016b568acc1cd0af05728af091
Chingly <kavisara.noya@mail.kmutt.ac.th>
2021-06-21 12:01:20 +0700

    [UPDATE] provider

--- shop_app/lib/widgets/products_grid.dart
+++ shop_app/lib/widgets/products_grid.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/models/product.dart';
+import 'package:shop_app/providers/products.dart';
+import 'package:shop_app/widgets/product_item.dart';
 
-class ProductsGrid extends StatelessWidget {
-  const ProductsGrid({Key? key}) : super(key: key);
-
+class ProductGrid extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
+    final productsData = Provider.of<Products>(context);
+    final products = productsData.items;
+    return GridView.builder(
+      padding: const EdgeInsets.all(10),
+      itemCount: products.length,
+      itemBuilder: (ctx, i) => ProductItem(
+        id: products[i].id,
+        title: products[i].title,
+        imageUrl: products[i].imageUrl,
+      ),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10),
     );
   }
 }
